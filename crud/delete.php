b98fb62b89d26cec2cef709578462e8d1e412942
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-02-04 14:56:20 +0100

    delete d

--- crud/delete.php
+++ crud/delete.php
@@ -1,2 +1,15 @@
 <?php
 include 'config.php';
+
+if(isset($_GET['id'])) {
+    $user_id = $_GET['id'];
+
+    $sql = "DELETE FROM users WHERE id=$user_id";
+
+    $result = $conn->query($sql);
+    if ($result == TRUE) {
+        echo "New deleted successfully";
+    } else {
+        echo "Error:".$sql."<br>".$conn->error;
+    }
+}
